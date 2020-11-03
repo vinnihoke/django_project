08957dded368edf57d459900cdde33b85ac17601
Vinni Hoke <vinni.hoke@gmail.com>
2020-11-02 17:33:30 -0800

    Tutorial 3-5

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# The request object must be present
+from .models import Post
 
 
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+def home(request):  # The request object must be present
+    context = {
+        'posts': Post.objects.all()
+    }
+
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
